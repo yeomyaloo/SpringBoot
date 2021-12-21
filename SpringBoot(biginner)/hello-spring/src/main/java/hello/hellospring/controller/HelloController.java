da2447ebe131d6ef3ce6f4a2bdd7db9928ea97a4
hwiju yeom <yeom456@gmail.com>
2021-12-21 16:35:17 +0900

    12/21
    SpringBoot

--- SpringBoot(biginner)/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ SpringBoot(biginner)/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model; //MVC 모델 - 뷰 - 컨트롤러 패턴에 M이 모델임
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,5 +14,35 @@ public class HelloController {
         model.addAttribute("data","hello!!");
         return "hello"; //
 
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody //바디부에 직접 내가 return 값을 넣어주겠다는 의미!!
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; //"hello spring" --> 네임에 넣는 것에 따라 달라짐. view 없이 그대로 문자가 내려감!!
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; //객체를 넣은 경우
+    }
+    static class Hello{ //static class로 넣어서 사용가능 자바에서 정식지원해주는 기능(내부클래스)
+        private String name;
+
+        // Alt + insert(ins) 버튼을 누르면 자동으로 만들어줌
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
