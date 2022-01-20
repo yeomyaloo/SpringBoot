1f25e9423a5bd484ad7815cc579f2ab2d7d4ebfd
hwiju yeom <81970382+yeomyaloo@users.noreply.github.com>
2022-01-20 18:25:00 +0900

    1/20

--- SpringBoot(bagic)/core/core/src/main/java/hello/core/MemberApp.java
+++ SpringBoot(bagic)/core/core/src/main/java/hello/core/MemberApp.java
@@ -3,14 +3,37 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSourceResolvable;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.ResolvableType;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.util.Locale;
+import java.util.Map;
 
 
 public class MemberApp {
     //psvm 치면 아래가 나옴
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-       //MemberService memberService = new MemberServiceImpl();
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+
+
+        //AppConfig안에 있는 것들은 스프링 컨테이너에 넣어서 객체 생성해주고 관리해줌.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
+
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
