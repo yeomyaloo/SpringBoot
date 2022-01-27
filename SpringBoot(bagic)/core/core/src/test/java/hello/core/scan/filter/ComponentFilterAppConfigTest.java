a05a3db982ec61fec590d39f0b538d61958e1cf3
hwiju yeom <yeom456@gmail.com>
2022-01-27 11:59:23 +0900

    1/27

---
+++ SpringBoot(bagic)/core/core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,37 @@
+package hello.core.scan.filter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Component;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void fileterScan(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+        assertThat(beanA).isNotNull();
+
+        Assertions.assertThrows(
+                NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("beanB", BeanB.class));
+    }
+    }
+
+
+    @ComponentScan(
+            includeFilters = @Filter(type = FilterType.ANNOTATION,classes = MyIncludeComponent.class),
+            excludeFilters = @Filter(type = FilterType.ANNOTATION,classes = MyExcludeComponent.class)
+    )
+    static class ComponentFilterAppConfig{
+
+    }
+}
--- SpringBoot(bagic)/core/core/src/test/java/hello/core/singleton/SingleTonTest.java
+++ SpringBoot(bagic)/core/core/src/test/java/hello/core/singleton/SingleTonTest.java
@@ -4,6 +4,8 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,4 +31,34 @@ public class SingleTonTest {
         assertThat(memberService1).isNotSameAs(memberService2);
     }
 
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        //SingletonService singletonService = new SingletonService();
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = "+singletonService1);
+        System.out.println("singletonService2 = "+singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        //Same = 인스턴스 비교
+        //isEqual = 자바의 equal
+    }
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainerTest(){
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService",MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService",MemberService.class);
+
+
+        //참조값이 다른 것을 확인
+        System.out.println("memberService1 = " +memberService1);
+        System.out.println("memberService2 = " +memberService2);
+
+        //memberService1 != memberService2
+        assertThat(memberService1).isSameAs(memberService2);
+    }
 }
