cf02346392091420e398f5e0e74c61a04a06e61c
hwiju yeom <yeom456@gmail.com>
2022-01-28 12:09:02 +0900

    1/28

--- SpringBoot(bagic)/core/core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
+++ SpringBoot(bagic)/core/core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -7,6 +7,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Component;
 
@@ -24,14 +25,16 @@ public class ComponentFilterAppConfigTest {
                 NoSuchBeanDefinitionException.class,
                 () -> ac.getBean("beanB", BeanB.class));
     }
-    }
-
-
+    @Configuration
     @ComponentScan(
-            includeFilters = @Filter(type = FilterType.ANNOTATION,classes = MyIncludeComponent.class),
-            excludeFilters = @Filter(type = FilterType.ANNOTATION,classes = MyExcludeComponent.class)
+            includeFilters = @Filter(type = FilterType.ANNOTATION, classes =
+                    MyIncludeComponent.class),
+            excludeFilters = @Filter(type = FilterType.ANNOTATION, classes =
+                    MyExcludeComponent.class)
     )
-    static class ComponentFilterAppConfig{
-
+    static class ComponentFilterAppConfig {
     }
 }
+
+
+
