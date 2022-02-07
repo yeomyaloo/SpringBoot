7281d0e10220709886d1760e85f45401694677ee
hwiju yeom <yeom456@gmail.com>
2022-02-07 13:24:33 +0900

    2/7
    spring scope

---
+++ SpringBoot(bagic)/core/core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,40 @@
+package hello.core.common;
+
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+
+//로그 출력을 위한 클래스 request를 사용
+@Component
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("["+uuid+"] "+"["+requestURL+"] "+message);
+    }
+
+    @PostConstruct
+    public void init(){
+        uuid = UUID.randomUUID().toString();
+        System.out.println("["+uuid+"] request scope bean create: "+this);
+
+    }
+
+    @PreDestroy
+    public void close(){
+        System.out.println("["+uuid+"] request scope bean close: "+this);
+    }
+}
