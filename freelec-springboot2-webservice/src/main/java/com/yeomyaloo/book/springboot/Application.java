0d112a376bdfdf424c2c856a0035f1a59896f06b
hwiju yeom <yeom456@gmail.com>
2022-02-19 15:51:28 +0900

    2/19

--- freelec-springboot2-webservice/src/main/java/com/yeomyaloo/book/springboot/Application.java
+++ freelec-springboot2-webservice/src/main/java/com/yeomyaloo/book/springboot/Application.java
@@ -2,9 +2,8 @@ package com.yeomyaloo.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing //JPA Auditing의 활성화를 위한 어노테이션
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- freelec-springboot2-webservice/src/main/java/com/yeomyaloo/book/springboot/web/IndexController.java
+++ freelec-springboot2-webservice/src/main/java/com/yeomyaloo/book/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.yeomyaloo.book.springboot.web;
 
 
+import com.yeomyaloo.book.springboot.config.auth.LoginUser;
 import com.yeomyaloo.book.springboot.config.auth.dto.SessionUser;
 import com.yeomyaloo.book.springboot.service.posts.PostsService;
 import com.yeomyaloo.book.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user!=null){
             model.addAttribute("userName", user.getName());
         }
