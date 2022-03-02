c196b29bdf257e0e9cbd52cd6993d017044ad027
hwiju yeom <yeom456@gmail.com>
2022-03-02 14:52:30 +0900

    3/2

---
+++ sevlet/src/test/java/hello/sevlet/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.sevlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
