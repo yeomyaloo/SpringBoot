24b337c427c33189d29bff63da55f9e050659c13
hwiju yeom <yeom456@gmail.com>
2022-04-14 14:02:41 +0900

    4/14

--- springMVC2(1)/thymleaf-basic/src/main/java/hello/thymleafbasic/basic/BasicController.java
+++ springMVC2(1)/thymleaf-basic/src/main/java/hello/thymleafbasic/basic/BasicController.java
@@ -80,11 +80,25 @@ public class BasicController {
         return "basic/date";
     }
 
-    //중요한 URL @{}
-    @GetMapping("/link")
+    //중요한 URL -  상대경로 /절대경로 @{}
+    @GetMapping("link")
     public String link(Model model){
         model.addAttribute("param1","data1");
         model.addAttribute("param2","data2");
         return "basic/link";
+    }
+
+    @GetMapping("/literal")
+    public String literal(Model model){
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
+
+    @GetMapping("/operation")
+    public String operation(Model model){
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+
+        return "basic/operation";
     }
 }
