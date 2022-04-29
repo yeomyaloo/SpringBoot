5f30f347abe0f4482266f054b940671fd3ea8c5a
hwiju yeom <81970382+yeomyaloo@users.noreply.github.com>
2022-04-29 18:55:57 +0900

    Create MessageCodesResolverTest.java

---
+++ springMVC2(4)/validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,18 @@
+package hello.itemservice.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.ObjectError;
+
+public class MessageCodesResolverTest {
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject(){
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        for (String messageCode : messageCodes){
+            System.out.println("messageCodes = " + messageCodes);
+        }
+    }
+}
