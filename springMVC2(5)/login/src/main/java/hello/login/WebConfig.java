3b26d4d204b2eafa2270d94066f7623e782f0d8d
hwiju yeom <yeom456@gmail.com>
2022-05-18 18:32:22 +0900

    5/18

---
+++ springMVC2(5)/login/src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,22 @@
+package hello.login;
+
+import hello.login.web.filter.Logfilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter(){
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new Logfilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
