b2634ea743f0d5cf2905441ccf1c8550881649b7
hwiju yeom <yeom456@gmail.com>
2022-06-09 02:31:48 +0900

    6/9
    layer 구분

---
+++ reactjs_springboot_aws(Book)/demo/src/main/java/com/example/demo/controller/TodoController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("todo")
+public class TodoController {
+
+    //빈을 알아서 찾고 그 빈을 이 인스턴스 멤버 변수로 연결하라는 뜻이다.
+    @Autowired
+    private TodoService service;
+
+    @GetMapping("/test")
+    public ResponseEntity<?> testTodo(){
+        String str = service.testService();//테스트 서비스 사용
+        List<String> list = new ArrayList<>();
+        list.add(str);
+
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(response);
+
+    }
+
+
+}
--- reactjs_springboot_aws(Book)/demo/src/main/java/com/example/demo/model/TodoEntity.java
+++ reactjs_springboot_aws(Book)/demo/src/main/java/com/example/demo/model/TodoEntity.java
@@ -5,6 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 //빌더 패턴 생성을 해주는 애너테이션
@@ -18,7 +24,19 @@ import lombok.NoArgsConstructor;
 
 // 멤버 변수의 setter/ getter 메서드 구현
 @Data
+
+//현재 클래스가 엔티티 클래스임을 명시해준다.
+@Entity
+
+//테이블 이름 지정
+@Table(name = "Todo")
 public class TodoEntity {
+    //기본키 지정
+    @Id
+    // ID를 자동생성하겠다는 의미이다. generator로 어떤 방식으로 ID를 자동생성할지 지정
+    @GeneratedValue(generator = "system-uuid")
+    //하이버네트가 제공하는 하는 것이 아닌 나만의 generator를 사용하고 싶을 때 사용한다.
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
     private String id; //이 오브젝트의 아이디
     private String userId; // 이 오브젝트를 생성한 사용자의 아이디
     private String title;
