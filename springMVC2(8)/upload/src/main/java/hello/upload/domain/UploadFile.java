14284abaae28d9d861b66fb6b8ba10f4d6bf927d
hwiju yeom <yeom456@gmail.com>
2022-06-10 19:40:26 +0900

    6/10

---
+++ springMVC2(8)/upload/src/main/java/hello/upload/domain/UploadFile.java
@@ -0,0 +1,18 @@
+package hello.upload.domain;
+
+
+import lombok.Data;
+
+@Data
+public class UploadFile {
+
+
+    private String uploadFileName; // 고객이 업로드한 파일명
+    private String storeFileName; //서버 내부 관리 파일명
+
+    public UploadFile(String uploadFileName, String storeFileName) {
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+    }
+
+}
